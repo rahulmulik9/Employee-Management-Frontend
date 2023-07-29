20f60bf0d8b857d49a44ed69574f6ebf18de64e3
Rahul Mulik <rahulmulik9@gmail.com>
2023-07-29 16:58:52 +0530

    New Employee form

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
     <div className="bg-gray-800">
       <div className="h-16 px-8 flex items-center">
-        <p className="text-white font-bold">Employee Managment</p>
+        <p className="text-white font-bold">Employee Management System</p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
