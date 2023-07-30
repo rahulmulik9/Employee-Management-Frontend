85e04a1bc5aef03b096303f0c3fa980ffbce0cb1
Rahul Mulik <rahulmulik9@gmail.com>
2023-07-30 16:06:52 +0530

    Update and delete functionality has been added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import AddEmployee from "./components/AddEmployee";
 import Navbar from "./components/Navbar";
 import EmployeeList from "./components/EmployeeList";
+import UpdateEmployee from "./components/UpdateEmployee";
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
           <Route path="/" element={<EmployeeList />}></Route>
           <Route path="/employeeList" element={<EmployeeList />} />
           <Route path="/addEmployee" element={<AddEmployee />} />
-          {/* <Route path="/editEmployee/:id" element={<UpdateEmployee />} /> */}
+          <Route path="/editEmployee/:id" element={<UpdateEmployee />} />
         </Routes>
       </BrowserRouter>
   );
